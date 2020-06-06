d1a37fa75d4ba13127a5d598733cd81636f10f27
Oskar Jansson <bullsaw1@gmail.com>
2020-06-06 14:35:45 +0200

    Update parse_data.py

--- parse_data.py
+++ parse_data.py
@@ -1,28 +1,68 @@
 import json
-import os
 
-def all_files() -> str:
+
+def all_paths() -> str:
     for path in ['ord_till_sjöss_data.txt']:
         yield path
 
+
 def get_db():
-    return json.load('ord_till_sjöss.json')
+    with open('ord_till_sjöss.json', 'r', encoding='utf8') as f:
+        return json.load(f)
+
 
 def add_to_db(db, data):
-    pass
+    added = 0
+    for new_word in data['words']:
+        add = True
+        for old_word in db['words']:
+            if new_word['word'] == old_word['word']:
+                add = False
+                break
+        if add:
+            added += 1
+            db['words'].append(new_word)
+    for new_category in data['categories']:
+        add = True
+        for old_category in db['categories']:
+            if new_category == old_category:
+                add = False
+                break
+        if add:
+            db['categories'].append(new_category)
+    print('Added %d new words' % added)
+    return db
+
 
 def save_db(db):
-    with open('ord_till_sjöss.json','w') as f:
+    with open('ord_till_sjöss.json', 'w') as f:
         f.seek(0)
         f.truncate()
-        f.write(json.dump(db))
+        json.dump(db, f)
+
+
+def parse_ord_till_sjoss(data):
+    d = {'words': [], 'categories': []}
+    for line in data:
+        if line and len(line) > 1:
+            words = line.split()
+            word = ''.join(words[:1])
+            description = ' '.join(words[1:])
+            description = description.lstrip('- ')
+            description = description.capitalize()
+            category = ''
+            d['words'].append({'word': word, 'description': description, 'category': category})
+            if category not in d['categories']:
+                d['categories'].append(category)
+    return d
+
 
 if __name__ == '__main__':
-    for path in all_files():
-        with open(path,'r') as f:
-            data = f.read()
+    for path in all_paths():
+        with open(path, 'r', encoding="utf8") as f:
+            data = f.read().split('\n')
         if path == 'ord_till_sjöss_data.txt':
-            json_data = parse_ordTillSjoss(data)
+            json_data = parse_ord_till_sjoss(data)
         db = get_db()
-        add_to_db(db, json_data)
+        db = add_to_db(db, json_data)
         save_db(db)
